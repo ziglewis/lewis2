cb37afbe38bdcef7dd60358ffed44de8f6d6b68a
ziglewis <ziglewis48@gmail.com>
2023-08-08 23:53:59 +0100

    wrote a function inside jsx and called it

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+    const myName = "ZigLewis"
+
+    const handleNameChanging = () => {
+      const names = ["John", "Ada", "Philip", "Junior", "Ifeoma", "Mathew"]
+      const randomName = Math.floor((Math.random()) * 6 )
+      return names[randomName]
+    }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
           I Zigede Lewis Junior set up React locally for the first time. Hurray!
         </p>
+        <p> For short call me {myName}</p>
+        <p>I wish to recommend these two random friends of mine for you: {handleNameChanging()} and {handleNameChanging()} for employment.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
