e9fe6f11a9751013dbe50af7f71b1232148df0d1
ziglewis <ziglewis48@gmail.com>
2023-08-08 13:47:27 +0100

    wrote my 1st line of reactCode on my local machine

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          I Zigede Lewis Junior set up React locally for the first time.
         </p>
         <a
           className="App-link"
